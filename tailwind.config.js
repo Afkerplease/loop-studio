8cc2b9c912125b93428f7c6ddb218659ace126d0
Moscow <afker2020@gmail.com>
2022-12-01 14:56:25 +0100

    setup

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        White: "hsl(0, 0%, 100%)",
+        Black: "hsl(0, 0%, 0%)",
+        DarkGray: "hsl(0, 0%, 55%)",
+        VeryDarkGray: "hsl(0, 0%, 41%)",
+      },
+      fontFamily: {
+        Atala: ["Alata"],
+        josephine: ["Josefin Sans"],
+      },
+    },
+  },
+  plugins: [],
+};
